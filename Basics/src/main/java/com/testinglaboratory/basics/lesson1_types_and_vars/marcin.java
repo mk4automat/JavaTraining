40d833063b5a68061a8999e7568c25878d2a8615
mk tester automatyczny <mk4testy@gmail.com>
2022-11-05 10:26:15 +0100

    Addition of marcin class printing variables

---
+++ Basics/src/main/java/com/testinglaboratory/basics/lesson1_types_and_vars/marcin.java
@@ -0,0 +1,9 @@
+package com.testinglaboratory.basics.lesson1_types_and_vars;
+
+public class marcin {
+    public static void main(String[] args) {
+        int myNumberOne = 5;
+        float myNumberTwo = 4.2f;
+        System.out.println(myNumberOne + myNumberTwo);
+    }
+}
